faa63f8199c8bfc8c2994317ea0766f2e5d9cddc
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-11-14 19:03:31 +0100

    create specific article page

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
 
 const ArticlePage = () => {
-  return (
-    <div>ArticlePage</div>
-  )
-}
+  const { articleId } = useParams();
 
-export default ArticlePage
+  const article = articles.find((article) => article.name === articleId);
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <h2>this is param id :{articleId}</h2>
+      {article.content.map((paragraph) => (
+        <p>{paragraph}</p>
+      ))}
+    </>
+  );
+};
+
+export default ArticlePage;
