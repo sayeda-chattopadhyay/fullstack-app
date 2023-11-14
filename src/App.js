67fca6391fdafe53960d1b2d59b697b17c70e358
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-11-14 18:32:08 +0100

    created pages and add route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My First fullstack app</h1>
-      <div id="page-body">
-        Welcome to my first fullstack app. This is a simple app that will allow
-        you to create a list of items.
+    <BrowserRouter>
+      <div className="App">
+        <h1>My First fullstack app</h1>
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ArticlesListPage = () => {
+  return (
+    <div>ArticlesListPage</div>
+  )
+}
+
+export default ArticlesListPage
