454febe307d517e237997d94d5edf5e0170b1c44
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-12-07 01:09:31 +0100

    change navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,37 @@ import { Link } from "react-router-dom";
 import { useLanguage } from "../LanguageContext";
 
 const NavBar = () => {
-  const { toggleLanguage } = useLanguage();
+  const { language, toggleLanguage } = useLanguage();
 
   return (
     <nav>
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/articles">Articles</Link>
-        </li>
+        {language === "en" ? (
+          <>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/articles">Articles</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/">hjem</Link>
+            </li>
+            <li>
+              <Link to="/about">Om</Link>
+            </li>
+            <li>
+              <Link to="/articles">artikler</Link>
+            </li>
+          </>
+        )}
+
         <li>
           <button onClick={toggleLanguage}>change language</button>
         </li>
